ad39bb6ce0f6bdecddf034dafe7aa02e53a5f1d5
Lewis Mwangi Wambugu <wambugulewis20@gmail.com>
2024-06-11 13:14:26 +0300

    Create control flow operators for the orm methods

--- lib/db/cli.py
+++ lib/db/cli.py
@@ -31,3 +31,36 @@ if __name__ == '__main__':
         print('12. Display Total Transactions')
         print('13. Display Total Transactions Per Category')
         print('0. Exit')
+
+        choice = input('Choose an option: ')
+        if choice == '1':
+            create_category()
+        elif choice == '2':
+            delete_category()
+        elif choice == '3':
+            display_all_categories()
+        elif choice == '4':
+            find_category_by_id()
+        elif choice == '5':
+            find_category_by_name()
+        elif choice == '6':
+            create_transaction()
+        elif choice == '7':
+            delete_transaction()
+        elif choice == '8':
+            display_all_transactions()
+        elif choice == '9':
+            find_transaction_by_id()
+        elif choice == '10':
+            find_transaction_by_description()
+        elif choice == '11':
+            display_total_categories()
+        elif choice == '12':
+            display_total_transactions()
+        elif choice == '13':
+            display_total_transactions_per_category()
+        elif choice == '0':
+            print('Goodbye! ✋🏼')
+            break
+        else:
+            print('Invalid choice. Please try again.')    
