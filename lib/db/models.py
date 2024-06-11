9dfbbaa6f837e19a5e606eb8a6897bbbfd9f5348
mrrsmburu <mrrsmburu@gmail.com>
2024-06-11 11:33:07 +0300

    setup the database

---
+++ lib/db/models.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, String, Float, Date, Enum, ForeignKey, func
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine('sqlite:///practice-finance.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
